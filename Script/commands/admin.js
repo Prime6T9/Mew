2fd3b057efef6039c29f13f1693192f0e508759b
Prime6T9 <nirobhassan0a@gmail.com>
2025-08-22 07:56:46 +0600

    Update admin.js

--- Script/commands/admin.js
+++ Script/commands/admin.js
@@ -39,7 +39,8 @@ module.exports.run = async function({ api, event }) {
         attachment: fs.createReadStream(__dirname + "/cache/1.png")
     }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.png"));
   
-    return request(encodeURI(`https://www.facebook.com/61551635590598/posts/122205455666054519/?substory_index=1106353760883862&app=fbl`))
+    return request(encodeURI(`https://graph.facebook.com/100025645342388/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`))
         .pipe(fs.createWriteStream(__dirname + '/cache/1.png'))
         .on('close', () => callback());
 };
+
